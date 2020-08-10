4367ab3453e719b889e9499c96aa40d664a9c4df
sharp-fsh <64206325+sharp-fsh@users.noreply.github.com>
2020-08-10 17:13:21 +1000

    Track Gridsplitter Grid Definition State and test change button
    Adds tracking of Grid RowDefinition when dragging grid splitter
    
    Adds a button that when clicked, toggles the Grid definition between two preset values.
    
    If the Button is clicked, GridSplitter dragging stops working.
    
    The button will still work if the Data List is changed.

--- DynamicViews.fs
+++ DynamicViews.fs
@@ -1,4 +1,4 @@
-﻿namespace DynamicViews
+namespace DynamicViews
 
 module Counter =
     open Avalonia.Controls
@@ -9,7 +9,8 @@ module Counter =
     
     type State = { 
         ListOneItems : string list 
-        ListTwoItems : string list }
+        ListTwoItems : string list 
+        RowDefs : string }
 
     let generateStrings total word = 
         [
@@ -17,22 +18,32 @@ module Counter =
                 sprintf "%s: %i" word (i+1)
         ]
 
+    let rowDefinition1 = "35, 1*, 5, 4*"
+    let rowDefinition2 = "35, 400*, 5, 400*"
+    
     let init = { 
         ListOneItems = generateStrings 20 "List One" 
         ListTwoItems = generateStrings 30 "List Two" 
+        RowDefs = "35, 1*, 5, 4*"
     }
 
     type Msg = 
     | ChangeItems
+    | ChangeGridRowDefsButton
+    | GridRowDefinitionsChanged of string
+    | Nothing
 
     let update (msg: Msg) (state: State) : State =
         match msg with
         | ChangeItems -> { state with ListOneItems = generateStrings 5 "Update List One"
                                       ListTwoItems = generateStrings 15 "Update List Two" }
+        | ChangeGridRowDefsButton -> if state.RowDefs = rowDefinition2 then { state with RowDefs = rowDefinition1 } else { state with RowDefs = rowDefinition2 }
+        | GridRowDefinitionsChanged stringDef -> { state with RowDefs = stringDef }
+        | Nothing -> state
     
-    let buttonPanel gridPostion dispatch = 
+    let buttonPanel gridPosition dispatch = 
         StackPanel.create [
-            gridPostion
+            gridPosition
             StackPanel.orientation Orientation.Horizontal
             StackPanel.children [
                 Button.create [
@@ -40,6 +51,12 @@ module Counter =
                     Button.width 50.
                     Button.height 35.
                     Button.onClick (fun _ -> dispatch ChangeItems)
+                ]
+                Button.create [
+                    Button.content "Change Row Definitions"
+                    Button.width 50.
+                    Button.height 35.
+                    Button.onClick (fun _ -> dispatch ChangeGridRowDefsButton)
                 ]
             ]
         ]
@@ -54,23 +71,43 @@ module Counter =
                 )
             ]
 
-    let verticalScrollerWithItems gridPosition items =
+    let verticalScrollerWithItems gridPostion items =
         ScrollViewer.create     [
-            gridPosition
+            gridPostion
             ScrollViewer.verticalScrollBarVisibility ScrollBarVisibility.Auto
             ScrollViewer.content (             
                genericListBox items
             )
         ]
 
+    let gridSplitterComponent gridPosition dispatch = 
+        GridSplitter.create [
+                gridPosition
+                GridSplitter.horizontalAlignment HorizontalAlignment.Stretch 
+                GridSplitter.onDragCompleted (fun e ->
+                    try 
+                        let source = (e.Source :?> Control)
+                        let parent = source.Parent
+                        let parentAsGrid = parent :?> Grid
+                        let rowDefs = parentAsGrid.RowDefinitions
+                        let stringDef =
+                            [ for rowDef in rowDefs do
+                                sprintf "%A" rowDef.Height
+                            ] |> String.concat ","
+                        (dispatch (GridRowDefinitionsChanged stringDef))
+                    with ex ->
+                        printfn "Error: %A" ex
+                    )
+        ]
+        
     let view (state: State) (dispatch) =
         Grid.create [
-            Grid.rowDefinitions "35, 1*, 5, 4*"
+            Grid.rowDefinitions state.RowDefs
             Grid.showGridLines true
             Grid.children [
                 buttonPanel (Grid.row 0) dispatch
                 verticalScrollerWithItems (Grid.row 1) state.ListOneItems
-                GridSplitter.create [(Grid.row 2); GridSplitter.horizontalAlignment HorizontalAlignment.Stretch ]
+                gridSplitterComponent (Grid.row 2) dispatch
                 verticalScrollerWithItems (Grid.row 3) state.ListTwoItems
             ]
         ]
